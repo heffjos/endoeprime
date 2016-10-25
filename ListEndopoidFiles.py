df3d8ce3b5296b07503a04a52214bf6f110a4b5d
Joseph Heffernan <heffjos@umich.edu>
2016-10-24 20:08:58 -0400

    ListEndopoidFiles.py now uses a namedtuple to store participant,run pairs.

--- ListEndopoidFiles.py
+++ ListEndopoidFiles.py
@@ -1,6 +1,9 @@
 import os
 import re
 import glob
+from collections import namedtuple
+
+ParRuns = namedtuple('ParRuns', ['Name', 'Runs'])
 
 def ListParticipants(MasterDir):
     Contents = os.listdir(MasterDir)
@@ -14,11 +17,12 @@ def ListTaskRuns(MasterDir, Participant, Task):
     else:
         Expected = ["run_01", "run_02", "run_03"]
    
-    ParRuns = glob.glob(os.path.join(MasterDir, Participant, 'func', Task))
-    ParRuns = [os.path.basename(x) for x in Runs]
-    for Run, Idx in enumerate(Expected):
-        if Run in ParRuns:
-            RunNums.append(Idx)
+    RunNums = []
+    Runs = glob.glob(os.path.join(MasterDir, Participant, 'func', Task, 'run*'))
+    Runs = [os.path.basename(x) for x in Runs]
+    for Idx, Run in enumerate(Expected):
+        if Run in Runs:
+            RunNums.append(Idx+1)
         else:
             RunNums.append("NA")
     return(RunNums)
@@ -26,32 +30,36 @@ def ListTaskRuns(MasterDir, Participant, Task):
 def PrintRuns(Participants, Task, OutFile):
     with open(OutFile, 'w') as Out:
         for OnePar in Participants:
-            print("{},".format(OnePar[0]), file=Out)
-            for RunIdx in range(len(OnePar[1])-1):
-                print("{},".format(OnePar[1][RunIdx]))
-            print("{}\n".format(OnePar[1][len(OnePar[1])-1))
+            print("{},".format(OnePar.Name), file=Out, end='')
+            NumRuns = len(OnePar.Runs)
+            print(" [", file=Out, end='')
+            for RunIdx in range(NumRuns-1):
+                print("{} ".format(OnePar.Runs[RunIdx]), file=Out, end='')
+            print("{}]\n".format(OnePar.Runs[NumRuns-1]), file=Out, end='')
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     Tasks = ["emotion", "visual", "verbal"]
     MasterDir = "/nfs/turbo/berent-lab/metabolic/Endopoid/Data/"
 
-    Emotion = ([], [])
-    Visual = ([], [])
-    Verbal = ([], [])
-
-    Emotion[0].extend(ListParticipants(MasterDir))
-    for OnePar in Emotion[0]:
-        Emotion[1].append(ListTaskRuns(MasterDir, OnePar, 'emotion'))
+    Emotion = []
+    for OnePar in ListParticipants(MasterDir):
+        Emotion.append(
+            ParRuns(OnePar, ListTaskRuns(MasterDir, OnePar, 'emotion'))
+        )
     PrintRuns(Emotion, 'emotion', 'AvailableRuns/Emotion.csv')
-    
-    Visual[0].extend(ListParticipants(MasterDir))
-    for OnePar in Visual[0]:
-        Visual[1].append(ListTaskRuns(MasterDir, OnePar, 'visual'))
+   
+    Visual = []
+    for OnePar in ListParticipants(MasterDir):
+        Visual.append(
+            ParRuns(OnePar, ListTaskRuns(MasterDir, OnePar, 'visual'))
+        ) 
     PrintRuns(Visual, 'visual', 'AvailableRuns/Visual.csv')
 
-    Verbal[0].extend(ListParticipants(MasterDir))
-    for OnePar in Verbal[0]:
-        Verbal[1].append(ListTaskRuns(MasterDir, OnePar, 'verbal'))
+    Verbal = []
+    for OnePar in ListParticipants(MasterDir):
+        Verbal.append(
+            ParRuns(OnePar, ListTaskRuns(MasterDir, OnePar, 'verbal'))
+        )
     PrintRuns(Verbal, 'verbal', 'AvailableRuns/Verbal.csv')
 
     
